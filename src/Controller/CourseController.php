384c4ac6a70715997f436a7da2ed3d846119ef60
arianr0d <tatyana.comolyh@yandex.ru>
2022-05-04 15:27:28 +0300

    исправлены роутинги + исправление шаблонов страниц

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Course;
+use App\Entity\Lesson;
 use App\Form\CourseType;
+use App\Form\LessonType;
 use App\Repository\CourseRepository;
+use App\Repository\LessonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -69,7 +72,7 @@ class CourseController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $courseRepository->add($course);
-            return $this->redirectToRoute('app_course_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_course_show', ['id' => $course->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('course/edit.html.twig', [
@@ -89,4 +92,28 @@ class CourseController extends AbstractController
 
         return $this->redirectToRoute('app_course_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    /**
+     * @Route("/{id}/lessons/newLesson/", name="app_lesson_new", methods={"GET", "POST"})
+     */
+    public function newLesson(Request $request, LessonRepository $lessonRepository, Course $course): Response
+    {
+        $lesson = new Lesson();
+        $lesson->setIdCourse($course);
+        $form = $this->createForm(LessonType::class, $lesson);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $lessonRepository->add($lesson);
+            return $this->redirectToRoute('app_course_show', ['id' => $course->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('lesson/new.html.twig', [
+            'lesson' => $lesson,
+            'form' => $form,
+            'course' => $course
+        ]);
+    }
 }
+
+
--- src/Controller/LessonController.php
+++ src/Controller/LessonController.php
@@ -16,36 +16,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class LessonController extends AbstractController
 {
     /**
-     * @Route("/", name="app_lesson_index", methods={"GET"})
-     */
-    public function index(LessonRepository $lessonRepository): Response
-    {
-        return $this->render('lesson/index.html.twig', [
-            'lessons' => $lessonRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="app_lesson_new", methods={"GET", "POST"})
-     */
-    public function new(Request $request, LessonRepository $lessonRepository): Response
-    {
-        $lesson = new Lesson();
-        $form = $this->createForm(LessonType::class, $lesson);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $lessonRepository->add($lesson);
-            return $this->redirectToRoute('app_lesson_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('lesson/new.html.twig', [
-            'lesson' => $lesson,
-            'form' => $form,
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="app_lesson_show", methods={"GET"})
      */
     public function show(Lesson $lesson): Response
